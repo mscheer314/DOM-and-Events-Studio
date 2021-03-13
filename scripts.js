6feac25ca086209605c4257fba9893d030409b15
Mark Scheer <mscheer314@gmail.com>
2021-03-12 19:30:04 -0600

    Updated

--- scripts.js
+++ scripts.js
@@ -1,2 +1,29 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init() {
+    const takeoffButton = document.getElementById("takeoff");
+    const landingButton = document.getElementById("landing");
+    const missionAbortButton = document.getElementById("missionAbort");
+    const upButton = document.getElementById("upButton");
+    const downButton = document.getElementById("downButton");
+    const leftButton = document.getElementById("leftButton");
+    const rightButton = document.getElementById("rightButton");
+
+    takeoffButton.onclick = () => {
+        if(window.confirm("Confirm that the shuttle is ready for takeoff.")) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            shuttleBackground.style.background = "blue";
+            spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) + 10000;
+        }
+    }
+    
+    landingButton.onclick = () => {
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        shuttleBackground.style.background = "green";
+        spaceShuttleHeight.innerHTML = 0;
+
+    }
+}
+
+window.addEventListener("load", init);
