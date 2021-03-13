c716e5aa124651df248ef8042135658c47e4171c
Mark Scheer <mscheer314@gmail.com>
2021-03-12 19:30:55 -0600

    Update

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ function init() {
     const downButton = document.getElementById("downButton");
     const leftButton = document.getElementById("leftButton");
     const rightButton = document.getElementById("rightButton");
+    const rocket = document.getElementById("rocket");
 
     takeoffButton.onclick = () => {
         if(window.confirm("Confirm that the shuttle is ready for takeoff.")) {
@@ -22,6 +23,31 @@ function init() {
         flightStatus.innerHTML = "The shuttle has landed.";
         shuttleBackground.style.background = "green";
         spaceShuttleHeight.innerHTML = 0;
+
+    }
+
+    missionAbortButton.onclick = () => {
+        if(window.confirm("Confirm that you want to abort the mission.")) {
+            flightStatus.innerHTML = "Mission aborted";
+            shuttleBackground.style.background = "green";
+            spaceShuttleHeight.innerHTML = 0;
+        }
+    }
+
+    const moveRocket(direction) => {
+        let movementUp = 0;
+        let movementDown = 0;
+        let movementRight = 0;
+        let movementLeft = 0;
+        
+        switch (direction) {
+            case up:
+                rocket.style.marginTop = Number(rocket.style.marginTop) - 10;
+        }
+    }
+
+    upButton.onclick = () => {
+        spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) + 10;
 
     }
 }
